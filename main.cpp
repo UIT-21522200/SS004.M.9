896673f0e3cd7075fdd9009edeb0652aae02d204
UIT-21520916 <105092593+UIT-21520916@users.noreply.github.com>
2022-05-16 17:13:38 +0700

    Create main.cpp
    Base code snake game

---
+++ main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <windows.h>
+#include <cstdlib>
+#include <conio.h>
+using namespace std;
+void gotoxy( int column, int line );
+struct Point{
+    int x,y;
+};
+class CONRAN{
+public:
+    struct Point A[100];
+    int DoDai;
+    CONRAN(){
+        DoDai = 3;
+        A[0].x = 10; A[0].y = 10;
+        A[1].x = 11; A[1].y = 10;
+        A[2].x = 12; A[2].y = 10;
+    }
+    void Ve(){
+        for (int i = 0; i < DoDai; i++){
+            gotoxy(A[i].x,A[i].y);
+            cout<<"X";
+        }
+    }
+    void DiChuyen(int Huong){
+        for (int i = DoDai-1; i>0;i--)
+            A[i] = A[i-1];
+        if (Huong==0) A[0].x = A[0].x + 1;
+        if (Huong==1) A[0].y = A[0].y + 1;
+        if (Huong==2) A[0].x = A[0].x - 1;
+        if (Huong==3) A[0].y = A[0].y - 1;
+
+    }
+};
+
+int main()
+{
+    CONRAN r;
+    int Huong = 0;
+    char t;
+
+    while (1){
+        if (kbhit()){
+            t = getch();
+            if (t=='a') Huong = 2;
+            if (t=='w') Huong = 3;
+            if (t=='d') Huong = 0;
+            if (t=='x') Huong = 1;
+        }
+        system("cls");
+        r.Ve();
+        r.DiChuyen(Huong);
+        Sleep(300);
+    }
+
+    return 0;
+}
+
+
+void gotoxy( int column, int line )
+  {
+  COORD coord;
+  coord.X = column;
+  coord.Y = line;
+  SetConsoleCursorPosition(
+    GetStdHandle( STD_OUTPUT_HANDLE ),
+    coord
+    );
+  }
+1
