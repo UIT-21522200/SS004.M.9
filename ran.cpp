74b245f12401a33c0fd1b8cd4a042f7b37117e01
UIT-21522328 <105094436+UIT-21522328@users.noreply.github.com>
2022-05-27 21:19:44 +0700

    Update ran.cpp

--- ran.cpp
+++ ran.cpp
@@ -2,7 +2,9 @@
 #include "mylib.h"
 #define MAX 100
 using namespace std;
-int sl = 7;
+
+int sl = 7; // do dai con ran
+
 //================ khu vuc khai bao nguyen mau ham ============
 void ve_tuong_tren();
 void ve_tuong_duoi();
@@ -21,172 +23,17 @@ bool kt_ran(int toadox[], int toadoy[]);
 void tao_qua(int& xqua, int& yqua, int toadox[], int toadoy[]);
 bool kt_ran_de_qua(int xqua, int yqua, int toadox[], int toadoy[]);
 bool kt_ran_an_qua(int xqua, int yqua, int x0, int y0);
-
-
-// ngo van manh them xu ly game over;
-void xu_ly_game_over(int toadox[], int toadoy[]) {
-	if (kt_ran(toadox, toadoy) == true) {
-
-		// xoa con ran vi no chet roi
-
-		for (int i = 0; i < sl; i++) {
-			gotoXY(toadox[i], toadoy[i]);
-			cout << " ";
-			Sleep(25);
-		}
-
-		// xoa tuong 
-
-		for (int i = 100; i >= 10; i--) {
-			gotoXY(i, 26);
-			cout << " ";
-			Sleep(0.5);
-		}
-		for (int i = 26; i >= 1; i--) {
-			gotoXY(10, i);
-			cout << " ";
-			Sleep(1);
-		}
-		for (int i = 10; i <= 100; i++) {
-			gotoXY(i, 1);
-			cout << " ";
-			Sleep(0.5);
-		}
-		for (int i = 1; i <= 26; i++) {
-			gotoXY(100, i);
-			cout << " ";
-			Sleep(1);
-		}
-
-		system("cls");
-		gotoXY(55, 13);
-		SetColor(124);
-		cout << "Game Over\n";
-		Sleep(1000);
-		SetColor(7);
-	}
-}
-
-//play gmae
-
-void play(int speed) {
-	bool gameover = false;
-	int toadox[MAX], toadoy[MAX];
-	ve_tuong();
-	khoi_tao_ran(toadox, toadoy);
-	ve_ran(toadox, toadoy);
-	//===================== kiem tra va tao qua ==============
-	srand(time(NULL));
-	int xqua = 0, yqua = 0;
-	//tao qua
-	tao_qua(xqua, yqua, toadox, toadoy);
-	int x = 50, y = 13;// dinh hinh vi tri can di chuyen cho ran
-	int check = 2;
-	while (gameover == false)
-	{
-		//system("cls");
-		//========= backspace
-		xoa_du_lieu_cu(toadox, toadoy);
-		// 0 : di xuong
-		//1: di len
-		//2: qua phai
-		//3: trai
-		//========= dieu khien
-		if (_kbhit())
-		{
-			char kitu = _getch();
-			if (kitu == -32)
-			{
-				kitu = _getch();
-				if (kitu == 72 && check != 0)//di len
-				{
-					check = 1;
-				}
-				else if (kitu == 80 && check != 1)//di xuong
-				{
-					check = 0;
-				}
-				else if (kitu == 77 && check != 3)//di phai
-				{
-					check = 2;
-				}
-				else if (kitu == 75 && check != 2)//di trai
-				{
-					check = 3;
-				}
-			}
-			else {
-				kitu = tolower(kitu);
-				if (kitu == 'w' && check != 0) check = 1;
-				else if (kitu == 's' && check != 1) check = 0;
-				else if (kitu == 'd' && check != 3) check = 2;
-				else if (kitu == 'a' && check != 2) check = 3;
-			}
-		}
-		//========= thuc hien di chuyen
-		if (check == 0)
-		{
-			y++;//di xuong
-		}
-		else if (check == 1)
-		{
-			y--;//di len
-		}
-		else if (check == 2)
-		{
-			x++;//di qua phai
-		}
-		else if (check == 3)
-		{
-			x--;//di qua trai	
-		}
-		xu_ly_ran(toadox, toadoy, x, y, xqua, yqua);
-		//========== kiem tra =========
-		gameover = kt_ran(toadox, toadoy);
-		Sleep(speed);
-	}
-	xu_ly_game_over(toadox, toadoy);
-}
-
-
-// menu game;
-void Menu() {
-	int lc = 1;
-	int speed = 150;
-	while (lc != 3) {
-		system("cls");
-		cout << "===============MENU===============";
-		cout << "\n|\t   1.Play                |";
-		cout << "\n|\t   2.Speed               |";
-		cout << "\n|\t   3.Quit                |";
-		cout << "\n===============MENU===============";
-		cout << endl << "Nhap lua chon cua ban: ";
-		cin >> lc;
-		system("cls");
-		if (lc == 1) play(speed);
-		else if (lc == 2) {
-			cout << "\n1.Nhanh"; // speed = 100;
-			cout << "\n2.Trung Binh"; // speed = 150;
-			cout << "\n3.Cham"; // speed = 200;
-			int choice = 2;
-			cin >> choice;
-			if (choice == 1) speed = 100;
-			else if (choice == 2) speed = 150;
-			else if (choice == 3) speed = 200;
-		}
-	}
-}
-
-
+void xu_ly_game_over(int toadox[], int toadoy[]);
+void play(int speed);
+void Menu();
 //============= ham main - xu ly chinh ==============
 int main()
 {
-	//---------------
 	Menu();
-	
 	return 0;
 }
-//============ khu vuc dinh nghia ham =============
+//============ khu vuc dinh nghia ham ===============
+
 void ve_tuong_tren()
 {
 	int x = 10, y = 1;
@@ -385,3 +232,158 @@ bool kt_ran_an_qua(int xqua, int yqua, int x0, int y0)
 	}
 	return false;
 }
+
+// ngo van manh them xu ly game over;
+
+void xu_ly_game_over(int toadox[], int toadoy[]) {
+	if (kt_ran(toadox, toadoy) == true) {
+
+		// xoa con ran vi no chet roi
+
+		for (int i = 0; i < sl; i++) {
+			gotoXY(toadox[i], toadoy[i]);
+			cout << " ";
+			Sleep(25);
+		}
+
+		// xoa tuong 
+
+		for (int i = 100; i >= 10; i--) {
+			gotoXY(i, 26);
+			cout << " ";
+			Sleep(0.5);
+		}
+		for (int i = 26; i >= 1; i--) {
+			gotoXY(10, i);
+			cout << " ";
+			Sleep(1);
+		}
+		for (int i = 10; i <= 100; i++) {
+			gotoXY(i, 1);
+			cout << " ";
+			Sleep(0.5);
+		}
+		for (int i = 1; i <= 26; i++) {
+			gotoXY(100, i);
+			cout << " ";
+			Sleep(1);
+		}
+
+		system("cls");
+		gotoXY(55, 13);
+		SetColor(124);
+		cout << "Game Over\n";
+		Sleep(1000);
+		SetColor(7);
+	}
+}
+
+//play gmae
+
+void play(int speed) {
+	bool gameover = false;
+	int toadox[MAX], toadoy[MAX];
+	ve_tuong();
+	khoi_tao_ran(toadox, toadoy);
+	ve_ran(toadox, toadoy);
+	//===================== kiem tra va tao qua ==============
+	srand(time(NULL));
+	int xqua = 0, yqua = 0;
+	//tao qua
+	tao_qua(xqua, yqua, toadox, toadoy);
+	int x = 50, y = 13;// dinh hinh vi tri can di chuyen cho ran
+	int check = 2;
+	while (gameover == false)
+	{
+		//system("cls");
+		//========= backspace
+		xoa_du_lieu_cu(toadox, toadoy);
+		// 0 : di xuong
+		//1: di len
+		//2: qua phai
+		//3: trai
+		//========= dieu khien
+		if (_kbhit())
+		{
+			char kitu = _getch();
+			if (kitu == -32)
+			{
+				kitu = _getch();
+				if (kitu == 72 && check != 0)//di len
+				{
+					check = 1;
+				}
+				else if (kitu == 80 && check != 1)//di xuong
+				{
+					check = 0;
+				}
+				else if (kitu == 77 && check != 3)//di phai
+				{
+					check = 2;
+				}
+				else if (kitu == 75 && check != 2)//di trai
+				{
+					check = 3;
+				}
+			}
+			else {
+				kitu = tolower(kitu);
+				if (kitu == 'w' && check != 0) check = 1;
+				else if (kitu == 's' && check != 1) check = 0;
+				else if (kitu == 'd' && check != 3) check = 2;
+				else if (kitu == 'a' && check != 2) check = 3;
+			}
+		}
+		//========= thuc hien di chuyen
+		if (check == 0)
+		{
+			y++;//di xuong
+		}
+		else if (check == 1)
+		{
+			y--;//di len
+		}
+		else if (check == 2)
+		{
+			x++;//di qua phai
+		}
+		else if (check == 3)
+		{
+			x--;//di qua trai	
+		}
+		xu_ly_ran(toadox, toadoy, x, y, xqua, yqua);
+		//========== kiem tra =========
+		gameover = kt_ran(toadox, toadoy);
+		Sleep(speed);
+	}
+	xu_ly_game_over(toadox, toadoy);
+}
+
+// menu game;
+void Menu() {
+	int lc = 1;
+	int speed = 150;
+	while (lc != 3) {
+		system("cls");
+		cout << "===============MENU===============";
+		cout << "\n|\t   1.Play                |";
+		cout << "\n|\t   2.Speed               |";
+		cout << "\n|\t   3.Quit                |";
+		cout << "\n===============MENU===============";
+		cout << endl << "Nhap lua chon cua ban: ";
+		cin >> lc;
+		system("cls");
+		if (lc == 1) play(speed);
+		else if (lc == 2) {
+			cout << "\n1.Nhanh"; // speed = 100;
+			cout << "\n2.Trung Binh"; // speed = 150;
+			cout << "\n3.Cham"; // speed = 200;
+			int choice = 2;
+			cin >> choice;
+			if (choice == 1) speed = 100;
+			else if (choice == 2) speed = 150;
+			else if (choice == 3) speed = 200;
+		}
+	}
+}
+
